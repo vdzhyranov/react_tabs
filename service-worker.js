45a0780d62c8a8fcb45b31f8fadad0671ad83e40
Viktor Dzhyranov <v.dzhyranov@gmail.com>
2021-07-23 10:32:25 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.cb55ce10f2eb2a4d10df16c6247cd130.js"
+  "./precache-manifest.2d6d9be4bfd4436441bfb2f82371ecdf.js"
 );
 
 self.addEventListener('message', (event) => {
